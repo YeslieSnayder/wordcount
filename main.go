888e5e8053699790c04bc7869c4334eca7e1044a
YeslieSnayder <kuzmickiy2000@mail.ru>
2021-12-21 15:03:50 +0300

    Added logic

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Print(0)
+		return
+	}
+	inp := strings.TrimSpace(os.Args[1])
+	if len(inp) == 0 {
+		fmt.Print(0)
+		return
+	}
+
+	l := len(strings.Split(inp, " "))
+	fmt.Print(l)
 }
